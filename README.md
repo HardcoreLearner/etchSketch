37652ea4a36a25d9e2214691c75cbfdd4388ee92
HardcoreLearner <MoBah@protonmail.com>
2022-07-26 20:33:47 +0000

    Planned for the code structure in JS. Created basic HTML structure. Added description of the project on Github

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # etchSketch
+
+This is a simple Javascript project to create a sketchpad only using
+Javascript, no HTML for body content.
+
+Also for the styling i'll be using CSS flexbox to have cool squared grid.
+
+Enjoy the project !
---
+++ script.js
@@ -0,0 +1,7 @@
+// Creation of 16 x 16 square divs nested in a div
+// Div creation using js 
+// Appearance as a grid via flexbox
+// create of an hover effect on all divs to make them change background color
+// Create a button to have a number of grid for example 32x32 maximum 100
+// Create a random mode to change the color arbitrarily
+// make each subsequent pass 10% closer to black
